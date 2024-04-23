ca55a0c9280f2aa73e9e42e95851f4140d3050ea
Natali Butikova <Butikova87@bk.ru>
2024-04-23 15:17:48 +0300

    change main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int amount = 13676; // стоимость билета
         int bonus = 20; // бонус на один рубль
 
-        System.out.println( "Ваш итоговый бонус за билет " + (amount * 1 / bonus) );
+        System.out.println("Сумма на счете " + (amount / bonus));
 
 
     }
